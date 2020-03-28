927b20c3af6092d4fc81988fac8c6de4b2af5443
Эрлихман Роман <roma.erlihman@mail.ru>
2020-03-28 16:14:41 +0500

    Реализованно добавление записей в БД

---
+++ MVC_Project/MusicDirectory/MusicDirectory/Controllers/NewRecordController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MusicDirectory.Models;
+
+namespace MusicDirectory.Controllers
+{
+    public class NewRecordController : Controller
+    {
+        private readonly ConnectionStringClass _csc;
+
+        public NewRecordController(ConnectionStringClass csc)
+        {
+            _csc = csc;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(MusClass mc)
+        {
+            _csc.Add(mc);
+            _csc.SaveChanges();
+            ViewBag.message = "Запись " + mc.FirstName + " успешно сохранена!";
+            return View(mc);
+        }
+    }
+}
---
+++ MVC_Project/MusicDirectory/MusicDirectory/Models/MusClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace MusicDirectory.Models
+{
+    public class MusClass  
+    {
+        [Key]
+        public int ID_Musician { get; set; }
+
+        [Required]
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string Patronymic { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public DateTime BirthDate { get; set; }
+    }
+}
